5b65fbbade027e3bfa1d24461b56454ff59e54f9
Caden Codes <cadenlcraddock@gmail.com>
2026-02-04 11:03:47 -0600

    Update RomanNumeralConverter.java

--- src/RomanNumerals/RomanNumeralConverter.java
+++ src/RomanNumerals/RomanNumeralConverter.java
@@ -10,18 +10,22 @@ public class RomanNumeralConverter
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in); // The 'input' variable calls the scanner.
+        int num;
 
-        System.out.print("Choose a number between 1 and 90: ");
-        int num = input.nextInt(); // The 'num' variable takes the user input for the integer.
-
-        if (num < 1 || num > 90) // This simply declines the user from choosing anything outside of the range 1--90
+        do
         {
-            System.out.println("Number must be between 1 and 90.");
-            return;
+            System.out.print("Choose a number between 1 and 90: ");
+            num = input.nextInt(); // The 'num' variable takes the user input for the integer.
+
+            if(num < 1 || num > 90)
+            {
+                System.out.println("Number must be between 1 and 90. Try again.");
+            }
         }
+        while (num < 1 || num > 90);
 
         StringBuilder roman = new StringBuilder(); // Steadily builds the string based on the given input. For example
-                                                   // 59 = 50 + 9 which is LIX or even 15 = 10 + 5 which is XV
+        // 59 = 50 + 9 which is LIX or even 15 = 10 + 5 which is XV
 
         while (num >= 90) { roman.append("XC"); num -= 90; }
         while (num >= 50) { roman.append("L");  num -= 50; }
